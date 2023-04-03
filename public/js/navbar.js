be37a7bf5b2063e87d33267e6ed966db13fa6ecd
LouisKwok92334 <87352246+LouisKwok92334@users.noreply.github.com>
2023-04-04 02:02:36 +0800

    Initial commit

---
+++ public/js/navbar.js
@@ -0,0 +1,88 @@
+const herder = document.querySelector("header");
+const bottom = document.querySelector("footer");
+
+function setHeader() {
+    herder.innerHTML += /*html*/ `
+        <div class="navbar-format-display">
+            <a href="./main.html">
+                <span class="navbar-title">CHANGE</span>
+            </a>
+            <a href="./main.html">
+                <div class="navbar-button">
+                    <span>Electronics</span>
+                </div>
+            </a>
+            <a href="./main.html">
+                <div class="navbar-button">
+                    <span>Fashion</span>
+                </div>
+            </a>
+            <a href="./main.html">
+                <div class="navbar-button">
+                    <span>Services</span>
+                </div>
+            </a>
+            <a href="./main.html">
+                <div class="navbar-button">
+                    <span>Cars</span>
+                </div>
+            </a>
+            <a href="./main.html">
+                <div class="navbar-button">
+                    <span>Property</span>
+                </div>
+            </a>
+            <a href="./main.html">
+                <div class="navbar-button">
+                    <span>All Property</span>
+                </div>
+            </a>
+            <div class="login-function">
+                <a href="./main.html">
+                    <div class="login-icon">
+                        <img src="../images/cat.png">
+                    </div>
+                </a>
+                <div class="login-button">
+                    <i class="fa-solid fa-right-to-bracket"></i>
+                </div>
+            </div>
+        </div>
+    `
+}
+function setFooter() {
+    bottom.innerHTML += /*html*/ `
+    <div class="Bottom">
+        <div class="Bottom-Introduce">
+            <img src="../images/Xiaoyuan.png">
+            <div class="Bottom-button">
+                <div>
+                    <a href="./index.html">最新消息</a>
+                </div>
+                <div>
+                    <a href="./character.html">角色設定</a>
+                </div>
+            </div>
+            <div class="Bottom-button">
+                <div>
+                    <a href="./strategy.html">遊戲攻略</a>
+                </div>
+                <div>
+                    <a href="./forums.html">討論區</a>
+                </div>
+            </div>
+            <div class="Bottom-button">
+                <div>
+                    <a href="./download.html">遊戲下載</a>
+                </div>
+                <div>
+                    <a href="./contact.html">聯絡我們</a>
+                </div>
+            </div>
+        </div>
+        <div class="Bottom-Regulations">© 2022 Xiaoyuan’ Adventures All rights reserved.</div>
+    </div>
+    `
+}
+
+// setHeader();
