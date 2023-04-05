3bd0b28d4c7b5090fac2820f11ac5d94c4505643
LouisKwok92334 <87352246+LouisKwok92334@users.noreply.github.com>
2023-04-06 02:44:52 +0800

    update main.html

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+const product = document.querySelector(".product-box-display");
+const productData = []
+
+function setProduct() {
+    for (let i = 0; i < productData.length; i++) {
+        
+    }
+}
