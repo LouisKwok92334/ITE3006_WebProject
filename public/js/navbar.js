0e2fbd8e20999a29aea1f65f1b74d94eff230566
LouisKwok92334 <87352246+LouisKwok92334@users.noreply.github.com>
2023-04-06 01:45:17 +0800

    created search bar

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -4,56 +4,60 @@ const bottom = document.querySelector("footer");
 function setHeader() {
     herder.innerHTML += /*html*/ `
         <div class="navbar-format-display">
-            <a href="./main.html">
-                <span class="navbar-title">CHANGE</span>
-            </a>
-            <a href="./main.html">
-                <div class="navbar-button">
-                    <span>Electronics</span>
-                </div>
-            </a>
-            <a href="./main.html">
-                <div class="navbar-button">
-                    <span>Fashion</span>
-                </div>
-            </a>
-            <a href="./main.html">
-                <div class="navbar-button">
-                    <span>Services</span>
-                </div>
-            </a>
-            <a href="./main.html">
-                <div class="navbar-button">
-                    <span>Cars</span>
-                </div>
-            </a>
-            <a href="./main.html">
-                <div class="navbar-button">
-                    <span>Property</span>
-                </div>
-            </a>
-            <a href="./main.html">
-                <div class="navbar-button">
-                    <span>All Property</span>
-                </div>
-            </a>
-            <div class="login-function">
+            <div class="navbar-button-format">
                 <a href="./main.html">
-                    <div class="login-icon">
-                        <img src="../images/cat.png">
+                    <span class="navbar-title">CHANGE</span>
+                </a>
+                <a href="./main.html">
+                    <div class="navbar-button">
+                        <span>Electronics</span>
                     </div>
                 </a>
                 <a href="./main.html">
-                    <i class="login-button fa-regular fa-comment"></i>
+                    <div class="navbar-button">
+                        <span>Fashion</span>
+                    </div>
                 </a>
                 <a href="./main.html">
-                    <i class="login-button fa-solid fa-right-to-bracket"></i>
+                    <div class="navbar-button">
+                        <span>Services</span>
+                    </div>
                 </a>
+                <a href="./main.html">
+                    <div class="navbar-button">
+                        <span>Cars</span>
+                    </div>
+                </a>
+                <a href="./main.html">
+                    <div class="navbar-button">
+                        <span>Property</span>
+                    </div>
+                </a>
+                <a href="./main.html">
+                    <div class="navbar-button">
+                        <span>All Property</span>
+                    </div>
+                </a>
+                <div class="login-function">
+                    <a href="./main.html">
+                        <div class="login-icon">
+                            <img src="../images/cat.png">
+                        </div>
+                    </a>
+                    <a href="./main.html">
+                        <i class="login-button fa-regular fa-comment"></i>
+                    </a>
+                    <a href="./main.html">
+                        <i class="login-button fa-solid fa-right-to-bracket"></i>
+                    </a>
+                </div>
             </div>
-        </div>
-        <div>
-            <div class="search-bar">
-                <input class="location-title" type="button" value="location">                
+            <div class="search-bar-display">
+                <div class="search-bar">
+                    <input class="location-button" type="button" value="Location">
+                    <input class="text-input" type="text" placeholder="🔍 Search for anything and everything">
+                    <input class="search-button" type="button" value="Search">                
+                </div>
             </div>
         </div>
     `
