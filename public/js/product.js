d19698de28c77e70ad8c43791e2e88a27525b09e
boscochuen011 <Bosco.chuen@gmail.com>
2023-04-06 21:54:11 +0800

    update

--- public/js/product.js
+++ public/js/product.js
@@ -10,26 +10,124 @@ const userData = [
     },
     {
         "name": "AngryCat",
-        "shortName": "IloVeGiRl",
+        "shortName": "@IloVeGiRl",
         "score": "★✩✩✩✩"
+    },
+    {
+        "name": "PlayfulDog",
+        "shortName": "@D.O.G",
+        "score": "★★✩✩✩"
+    },
+    {
+        "name": "CuriousSquirrel",
+        "shortName": "@SqrlWhisperer",
+        "score": "★★★★★"
+    },
+    {
+        "name": "ChirpyBird",
+        "shortName": "@BirdLover",
+        "score": "★★★✩✩"
+    },
+    {
+        "name": "WiseOwl",
+        "shortName": "@NightThinker",
+        "score": "★★★★✩"
+    },
+    {
+        "name": "LazyPanda",
+        "shortName": "@BambooFan",
+        "score": "★★✩✩✩"
+    },
+    {
+        "name": "EnergeticKangaroo",
+        "shortName": "@JumpMaster",
+        "score": "★★★★★"
+    },
+    {
+        "name": "FearlessTiger",
+        "shortName": "@RoarLoud",
+        "score": "★★★★✩"
+    },
+    {
+        "name": "FriendlyDolphin",
+        "shortName": "@SeaExplorer",
+        "score": "★★★✩✩"
     }
 ]
+
 const productData = [
     {
         "name": "Apple Macbook Air",
-        "price": 160,
+        "price": 1200,
         "photo": "Macbook.jpg",
         "type": "Electronics",
         "userID": 2
     },
     {
-        "name": "",
-        "price": 0,
-        "photo": ".jpg",
+        "name": "Nike Running Shoes",
+        "price": 100,
+        "photo": "nikeShoes.jpg",
         "type": "Fashion",
         "userID": 1
+    },
+    {
+        "name": "Home Cleaning Service",
+        "price": 80,
+        "photo": "cleaningService.jpg",
+        "type": "Services",
+        "userID": 3
+    },
+    {
+        "name": "Toyota Corolla",
+        "price": 18000,
+        "photo": "toyotaCorolla.jpg",
+        "type": "Cars",
+        "userID": 4
+    },
+    {
+        "name": "2 Bedroom Apartment",
+        "price": 250000,
+        "photo": "apartment.jpg",
+        "type": "Property",
+        "userID": 5
+    },
+    {
+        "name": "Herschel Wallet",
+        "price": 30,
+        "photo": "herschelWallet.jpg",
+        "type": "Fashion",
+        "userID": 10
+    },
+    {
+        "name": "Samsung Galaxy S22",
+        "price": 900,
+        "photo": "galaxyS22.jpg",
+        "type": "Electronics",
+        "userID": 7
+    },
+    {
+        "name": "Gucci Handbag",
+        "price": 1200,
+        "photo": "gucciHandbag.jpg",
+        "type": "Fashion",
+        "userID": 8
+    },
+    {
+        "name": "Personal Trainer",
+        "price": 50,
+        "photo": "personalTrainer.jpg",
+        "type": "Services",
+        "userID": 9
+    },
+    {
+        "name": "Honda Civic",
+        "price": 22000,
+        "photo": "hondaCivic.jpg",
+        "type": "Cars",
+        "userID": 10
     }
 ]
+
 
 function setProduct() {
     for (let i = 0; i < productData.length; i++) {
