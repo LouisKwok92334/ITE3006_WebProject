1c8f960744d63dce393559520ec5908f8bd85847
LouisKwok92334 <87352246+LouisKwok92334@users.noreply.github.com>
2023-04-06 19:13:52 +0800

    update

--- public/js/product.js
+++ public/js/product.js
@@ -1,3 +1,26 @@
-let productParams = new URLSearchParams(window.location.search);
-let productID = urlParams.get("id");
+// let productParams = new URLSearchParams(window.location.search);
+// let productID = urlParams.get("id");
 
+const product = document.querySelector(".product-box-display");
+const productData = [
+    {
+        "name": "Apple Macbook Air",
+        "price": 160,
+        "img": "Macbook.jpg",
+        "type": "Electronics",
+        "user": "Bosco"
+    },
+    {
+        "name": "",
+        "price": 0,
+        "img": ".jpg",
+        "type": "Fashion",
+        "user": "Louis"
+    }
+]
+
+function setProduct() {
+    for (let i = 0; i < productData.length; i++) {
+        
+    }
+}
