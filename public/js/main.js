318d10df8dfad3b6d7d70092b735778f4e2387fc
LouisKwok92334 <87352246+LouisKwok92334@users.noreply.github.com>
2023-04-06 19:11:49 +0800

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,20 @@
 const product = document.querySelector(".product-box-display");
-const productData = []
+const productData = [
+    {
+        "name": "Apple Macbook Air",
+        "price": 160,
+        "img": "Macbook.jpg",
+        "type": "Electronics",
+        "user": "Bosco"
+    },
+    {
+        "name": "",
+        "price": 0,
+        "img": ".jpg",
+        "type": "Fashion",
+        "user": "Louis"
+    }
+]
 
 function setProduct() {
     for (let i = 0; i < productData.length; i++) {
